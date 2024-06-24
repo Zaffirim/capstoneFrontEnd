dea550dab8220e78484d3e1e74de6ee990b781ab
Milo <zaffirim@gmail.com>
2024-06-24 11:56:53 +0200

    home component e navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const rotte: Route[] = [
+  {
+    path: '', 
+    component: HomeComponent,
+  },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rotte),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
