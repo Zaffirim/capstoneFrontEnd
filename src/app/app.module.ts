40fe1465df4e9aa8d8f6bf773e4c222d70378a7f
Milo <zaffirim@gmail.com>
2024-06-24 13:32:50 +0200

    feat: implement footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const rotte: Route[] = [
@@ -20,7 +21,8 @@ const rotte: Route[] = [
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
